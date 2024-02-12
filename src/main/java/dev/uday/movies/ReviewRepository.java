597a9fff89074fabc69daf5c8242f35188bc8d57
Uday Mahajan <udaymahajan512@gmail.com>
2024-02-11 20:13:19 -0800

    Backend Completed

---
+++ src/main/java/dev/uday/movies/ReviewRepository.java
@@ -0,0 +1,11 @@
+package dev.uday.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+
+}
